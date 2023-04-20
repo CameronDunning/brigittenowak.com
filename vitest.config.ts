4fcc0788f85316d42c19cb85e951c6915635ad8a
Cameron <cdunning@avalanche.ca>
2023-04-19 22:48:53 -0700

    formatting

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts',
-  }
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/tests/setup.ts',
+    },
 })
