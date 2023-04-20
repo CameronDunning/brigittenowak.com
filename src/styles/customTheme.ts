a0dc4aa195b8d822deae4d50a2f79cc46e46e9a7
Cameron <cdunning@avalanche.ca>
2023-04-19 22:49:29 -0700

    formatting

--- src/styles/customTheme.ts
+++ src/styles/customTheme.ts
@@ -1,93 +1,80 @@
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
-  colors: {
-    primary: {
-      50: "#f6f4ef",
-      100: "#e8e2d3",
-      200: "#dad2b8",
-      300: "#ccc29d",
-      400: "#daccb4", // main color
-      500: "#b6b08f",
-      600: "#91876c",
-      700: "#6c5e48",
-      800: "#473725",
-      900: "#1f140a",
-    },
-  },
-  fonts: {
-    heading: "Helvetica Neue, sans-serif",
-    body: "Open Sans, sans-serif",
-  },
-  styles: {
-    global: {
-      body: {
-        bg: "#59728e",
-        color: "primary.400",
-        fontFamily: "body",
-        fontWeight: "normal",
-        lineHeight: "base",
-        paddingBottom: "40px",
-      },
-      a: {
-        color: "primary.400",
-        textDecoration: "none",
-        _hover: {
-          textDecoration: "underline",
+    colors: {
+        primary: {
+            50: '#f6f4ef',
+            100: '#e8e2d3',
+            200: '#dad2b8',
+            300: '#ccc29d',
+            400: '#daccb4', // main color
+            500: '#b6b08f',
+            600: '#91876c',
+            700: '#6c5e48',
+            800: '#473725',
+            900: '#1f140a',
         },
-      },
-      h1: {
-        fontFamily: "heading",
-        fontSize: "5xl",
-        fontWeight: "bold",
-        color: "primary.400",
-      },
-      h2: {
-        fontFamily: "heading",
-        fontSize: "4xl",
-        fontWeight: "bold",
-        color: "primary.400",
-      },
-      h3: {
-        fontFamily: "heading",
-        fontSize: "3xl",
-        fontWeight: "bold",
-        color: "primary.400",
-      },
-      h4: {
-        fontFamily: "heading",
-        fontSize: "2xl",
-        fontWeight: "bold",
-        color: "primary.400",
-      },
-      h5: {
-        fontFamily: "heading",
-        fontSize: "xl",
-        fontWeight: "bold",
-        color: "primary.400",
-      },
-      h6: {
-        fontFamily: "heading",
-        fontSize: "lg",
-        fontWeight: "bold",
-        color: "primary.400",
-      },
-      ul: {
-        listStyleType: "none",
-      },
-      li: {
-        marginBottom: "0.5rem",
-        display: "flex",
-        alignItems: "center",
-        "&::before": {
-          content: "'\\2022'",
-          color: "primary.400",
-          fontWeight: "bold",
-          display: "inline-block",
-          width: "1rem",
-          marginLeft: "-1rem",
-        },
-      },
     },
-  },
+    fonts: {
+        heading: 'Helvetica Neue, sans-serif',
+        body: 'Open Sans, sans-serif',
+    },
+    styles: {
+        global: {
+            body: {
+                bg: '#59728e',
+                color: 'primary.400',
+                fontFamily: 'body',
+                fontWeight: 'normal',
+                lineHeight: 'base',
+                paddingBottom: '40px',
+            },
+            a: {
+                color: 'primary.400',
+                textDecoration: 'none',
+                _hover: {
+                    textDecoration: 'underline',
+                },
+            },
+            h1: {
+                fontFamily: 'heading',
+                fontSize: '5xl',
+                fontWeight: 'bold',
+                color: 'primary.400',
+            },
+            h2: {
+                fontFamily: 'heading',
+                fontSize: '4xl',
+                fontWeight: 'bold',
+                color: 'primary.400',
+            },
+            h3: {
+                fontFamily: 'heading',
+                fontSize: '3xl',
+                fontWeight: 'bold',
+                color: 'primary.400',
+            },
+            h4: {
+                fontFamily: 'heading',
+                fontSize: '2xl',
+                fontWeight: 'bold',
+                color: 'primary.400',
+            },
+            h5: {
+                fontFamily: 'heading',
+                fontSize: 'xl',
+                fontWeight: 'bold',
+                color: 'primary.400',
+            },
+            h6: {
+                fontFamily: 'heading',
+                fontSize: 'lg',
+                fontWeight: 'bold',
+                color: 'primary.400',
+            },
+            ul: {
+                listStyleType: 'none',
+            },
+        },
+    },
 })
