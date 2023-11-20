f60c57018099ec21a4901c7ea57703350689615d
Cameron <cdunning@avalanche.ca>
2023-11-20 00:43:31 -0800

    Adds router link to navbar links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,7 +11,7 @@ type NavLinkProps = {
 const links = ['About', 'Artwork', 'Representation', 'Contact']
 
 const NavLink = ({ children }: NavLinkProps) => (
-    <Link pl={{ base: 2, md: 0 }} href={`/${children.toLowerCase()}`}>
+    <Link as={RouterLink} pl={{ base: 2, md: 0 }} href={`/${children.toLowerCase()}`}>
         {children}
     </Link>
 )
