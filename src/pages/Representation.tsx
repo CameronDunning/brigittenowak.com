d7085a4bbe3fb63be5ec2528788550761802f7b2
Cameron <cdunning@avalanche.ca>
2024-02-10 10:28:10 -0800

    Update gallery order

--- src/pages/Representation.tsx
+++ src/pages/Representation.tsx
@@ -5,12 +5,11 @@ export const Representation = () => {
         <Flex as="main" w={{ base: '100%', md: '4xl' }} flexDirection="column">
             <Box as="section" px={{ base: 2, md: 0 }}>
                 <Heading pb={2}>Galleries</Heading>
-                <GalleryInfo name="ArtMatch.ca" url="www.artmatch.ca/" />
-                <GalleryInfo name="Ethel Curry Gallery" url="www.theethelcurrygallery.com" location="Haliburton, Ontario" phone="705-457-9687" />
-                <GalleryInfo name="Latitude 44 Gallery" url="www.latitude44gallery.ca" location="Toronto, Ontario" />
+                <GalleryInfo name="Red Canoe Gallery" url="www.redcanoegallery.com" location="Bala, Ontario" />
                 <GalleryInfo name="Koyman Galleries" url="www.koymangalleries.com" location="Ottawa, Ontario" />
-                <GalleryInfo name="Red Canoe Gallery" url="www.redcanoegallery.com" location="Pt. Carling, Ontario" />
                 <GalleryInfo name="St. Germain Gallery" url="www.stgermaingallery.com" location="Toronto, Ontario" />
+                <GalleryInfo name="Latitude 44 Gallery" url="www.latitude44gallery.ca" location="Toronto, Ontario" />
+                <GalleryInfo name="ArtMatch.ca" url="www.artmatch.ca/" />
             </Box>
         </Flex>
     )
